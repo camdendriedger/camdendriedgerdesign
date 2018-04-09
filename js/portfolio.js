0218fdea20f083f781998f6188a7a0fb638a9cd0
Camden Driedger <camdendriedger@gmail.com>
2018-04-09 00:27:17 -0400

    Upload 3.0
    added flex grid, general updates and mailchimp connection

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,4 @@
-    $(document).ready(function(){
+    $(document).ready(function () {
 
 
 
@@ -7,28 +7,28 @@
 
 
 
-   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+    	// Add smooth scrolling to all links
+    	$("a").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    		// Make sure this.hash has a value before overriding default behavior
+    		if (this.hash !== "") {
+    			// Prevent default anchor click behavior
+    			event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+    			// Store hash
+    			var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+    			// Using jQuery's animate() method to add smooth page scroll
+    			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    			$('html, body').animate({
+    				scrollTop: $(hash).offset().top
+    			}, 800, function () {
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+    				// Add hash (#) to URL when done scrolling (default click behavior)
+    				window.location.hash = hash;
+    			});
+    		} // End if
+    	});
 
 
 
@@ -38,124 +38,124 @@
 
 
 
-//branding 1
+    	//branding 1
 
 
-        $('#logo1js').click(function() {
-    $('#wrapper-1-1').toggle();
-});
+    	$('#logo1js').click(function () {
+    		$('#wrapper-1-1').toggle();
+    	});
 
-        $('#close-button-1-1').click(function() {
-    $('#wrapper-1-1').toggle();
-});
+    	$('#close-button-1-1').click(function () {
+    		$('#wrapper-1-1').toggle();
+    	});
 
 
-//branding 2
+    	//branding 2
 
 
-   $('#logo2js').click(function() {
-    $('#wrapper-1-2').toggle();
-});
+    	$('#logo2js').click(function () {
+    		$('#wrapper-1-2').toggle();
+    	});
 
-        $('#close-button-1-2').click(function() {
-    $('#wrapper-1-2').toggle();
-});
+    	$('#close-button-1-2').click(function () {
+    		$('#wrapper-1-2').toggle();
+    	});
 
 
 
 
-//branding 3
+    	//branding 3
 
 
-        $('#logo3js').click(function() {
-    $('#wrapper-1-3').toggle();
-});
+    	$('#logo3js').click(function () {
+    		$('#wrapper-1-3').toggle();
+    	});
 
-        $('#close-button-1-3').click(function() {
-    $('#wrapper-1-3').toggle();
-});
+    	$('#close-button-1-3').click(function () {
+    		$('#wrapper-1-3').toggle();
+    	});
 
 
-//branding 4
+    	//branding 4
 
 
-   $('#logo4js').click(function() {
-    $('#wrapper-1-4').toggle();
-});
+    	$('#logo4js').click(function () {
+    		$('#wrapper-1-4').toggle();
+    	});
 
-        $('#close-button-1-4').click(function() {
-    $('#wrapper-1-4').toggle();
-});
+    	$('#close-button-1-4').click(function () {
+    		$('#wrapper-1-4').toggle();
+    	});
 
 
 
 
-//branding 5
+    	//branding 5
 
 
-        $('#logo5js').click(function() {
-    $('#wrapper-1-5').toggle();
-});
+    	$('#logo5js').click(function () {
+    		$('#wrapper-1-5').toggle();
+    	});
 
-        $('#close-button-1-5').click(function() {
-    $('#wrapper-1-5').toggle();
-});
+    	$('#close-button-1-5').click(function () {
+    		$('#wrapper-1-5').toggle();
+    	});
 
 
 
-//branding 6
+    	//branding 6
 
 
-   $('#logo6js').click(function() {
-    $('#wrapper-1-6').toggle();
-});
+    	$('#logo6js').click(function () {
+    		$('#wrapper-1-6').toggle();
+    	});
 
-        $('#close-button-1-6').click(function() {
-    $('#wrapper-1-6').toggle();
-});
+    	$('#close-button-1-6').click(function () {
+    		$('#wrapper-1-6').toggle();
+    	});
 
 
 
 
-//branding 7
+    	//branding 7
 
 
-   $('#logo7js').click(function() {
-    $('#wrapper-1-7').toggle();
-});
+    	$('#logo7js').click(function () {
+    		$('#wrapper-1-7').toggle();
+    	});
 
-        $('#close-button-1-7').click(function() {
-    $('#wrapper-1-7').toggle();
-});
+    	$('#close-button-1-7').click(function () {
+    		$('#wrapper-1-7').toggle();
+    	});
 
 
 
 
-//branding 8
+    	//branding 8
 
 
-   $('#logo8js').click(function() {
-    $('#wrapper-1-8').toggle();
-});
+    	$('#logo8js').click(function () {
+    		$('#wrapper-1-8').toggle();
+    	});
 
-        $('#close-button-1-8').click(function() {
-    $('#wrapper-1-8').toggle();
-});
+    	$('#close-button-1-8').click(function () {
+    		$('#wrapper-1-8').toggle();
+    	});
 
 
 
 
 
-//branding 9
+    	//branding 9
 
 
-   $('#logo9js').click(function() {
-    $('#wrapper-1-9').toggle();
-});
+    	$('#logo9js').click(function () {
+    		$('#wrapper-1-9').toggle();
+    	});
 
-        $('#close-button-1-9').click(function() {
-    $('#wrapper-1-9').toggle();
-});
+    	$('#close-button-1-9').click(function () {
+    		$('#wrapper-1-9').toggle();
+    	});
 
 
 
@@ -165,203 +165,203 @@
 
 
 
-//print
+    	//print
 
 
 
 
 
 
-//print 1
+    	//print 1
 
 
-        $('#print1js').click(function() {
-    $('#wrapper-2-1').toggle();
-});
+    	$('#print1js').click(function () {
+    		$('#wrapper-2-1').toggle();
+    	});
 
-        $('#close-button-2-1').click(function() {
-    $('#wrapper-2-1').toggle();
-});
+    	$('#close-button-2-1').click(function () {
+    		$('#wrapper-2-1').toggle();
+    	});
 
 
 
 
 
-//print 2
+    	//print 2
 
 
-        $('#print2js').click(function() {
-    $('#wrapper-2-2').toggle();
-});
+    	$('#print2js').click(function () {
+    		$('#wrapper-2-2').toggle();
+    	});
 
-        $('#close-button-2-2').click(function() {
-    $('#wrapper-2-2').toggle();
-});
+    	$('#close-button-2-2').click(function () {
+    		$('#wrapper-2-2').toggle();
+    	});
 
 
 
 
-//print 3
+    	//print 3
 
 
-        $('#print3js').click(function() {
-    $('#wrapper-2-3').toggle();
-});
+    	$('#print3js').click(function () {
+    		$('#wrapper-2-3').toggle();
+    	});
 
-        $('#close-button-2-3').click(function() {
-    $('#wrapper-2-3').toggle();
-});
+    	$('#close-button-2-3').click(function () {
+    		$('#wrapper-2-3').toggle();
+    	});
 
 
 
-//print 4
+    	//print 4
 
 
-        $('#print4js').click(function() {
-    $('#wrapper-2-4').toggle();
-});
+    	$('#print4js').click(function () {
+    		$('#wrapper-2-4').toggle();
+    	});
 
-        $('#close-button-2-4').click(function() {
-    $('#wrapper-2-4').toggle();
-});
+    	$('#close-button-2-4').click(function () {
+    		$('#wrapper-2-4').toggle();
+    	});
 
 
 
-//print 5
+    	//print 5
 
 
-        $('#print5js').click(function() {
-    $('#wrapper-2-5').toggle();
-});
+    	$('#print5js').click(function () {
+    		$('#wrapper-2-5').toggle();
+    	});
 
-        $('#close-button-2-5').click(function() {
-    $('#wrapper-2-5').toggle();
-});
+    	$('#close-button-2-5').click(function () {
+    		$('#wrapper-2-5').toggle();
+    	});
 
 
 
 
-//print 6
+    	//print 6
 
 
-        $('#print6js').click(function() {
-    $('#wrapper-2-6').toggle();
-});
+    	$('#print6js').click(function () {
+    		$('#wrapper-2-6').toggle();
+    	});
 
-        $('#close-button-2-6').click(function() {
-    $('#wrapper-2-6').toggle();
-});
+    	$('#close-button-2-6').click(function () {
+    		$('#wrapper-2-6').toggle();
+    	});
 
 
 
 
 
-//print 7
+    	//print 7
 
 
-        $('#print7js').click(function() {
-    $('#wrapper-2-7').toggle();
-});
+    	$('#print7js').click(function () {
+    		$('#wrapper-2-7').toggle();
+    	});
 
-        $('#close-button-2-7').click(function() {
-    $('#wrapper-2-7').toggle();
-});
+    	$('#close-button-2-7').click(function () {
+    		$('#wrapper-2-7').toggle();
+    	});
 
 
 
 
 
-//print 8
+    	//print 8
 
 
-        $('#print8js').click(function() {
-    $('#wrapper-2-8').toggle();
-});
+    	$('#print8js').click(function () {
+    		$('#wrapper-2-8').toggle();
+    	});
 
-        $('#close-button-2-8').click(function() {
-    $('#wrapper-2-8').toggle();
-});
+    	$('#close-button-2-8').click(function () {
+    		$('#wrapper-2-8').toggle();
+    	});
 
 
 
 
-//print 9
+    	//print 9
 
 
-        $('#print9js').click(function() {
-    $('#wrapper-2-9').toggle();
-});
+    	$('#print9js').click(function () {
+    		$('#wrapper-2-9').toggle();
+    	});
 
-        $('#close-button-2-9').click(function() {
-    $('#wrapper-2-9').toggle();
-});
+    	$('#close-button-2-9').click(function () {
+    		$('#wrapper-2-9').toggle();
+    	});
 
 
 
-//print 10
+    	//print 10
 
 
-        $('#print10js').click(function() {
-    $('#wrapper-2-10').toggle();
-});
+    	$('#print10js').click(function () {
+    		$('#wrapper-2-10').toggle();
+    	});
 
-        $('#close-button-2-10').click(function() {
-    $('#wrapper-2-10').toggle();
-});
+    	$('#close-button-2-10').click(function () {
+    		$('#wrapper-2-10').toggle();
+    	});
 
 
 
-//print 11
+    	//print 11
 
 
-        $('#print11js').click(function() {
-    $('#wrapper-2-11').toggle();
-});
+    	$('#print11js').click(function () {
+    		$('#wrapper-2-11').toggle();
+    	});
 
-        $('#close-button-2-11').click(function() {
-    $('#wrapper-2-11').toggle();
-});
+    	$('#close-button-2-11').click(function () {
+    		$('#wrapper-2-11').toggle();
+    	});
 
 
 
 
-//print 12
+    	//print 12
 
 
-        $('#print12js').click(function() {
-    $('#wrapper-2-12').toggle();
-});
+    	$('#print12js').click(function () {
+    		$('#wrapper-2-12').toggle();
+    	});
 
-        $('#close-button-2-12').click(function() {
-    $('#wrapper-2-12').toggle();
-});
+    	$('#close-button-2-12').click(function () {
+    		$('#wrapper-2-12').toggle();
+    	});
 
 
 
-//print 13
+    	//print 13
 
 
-        $('#print13js').click(function() {
-    $('#wrapper-2-13').toggle();
-});
+    	$('#print13js').click(function () {
+    		$('#wrapper-2-13').toggle();
+    	});
 
-        $('#close-button-2-13').click(function() {
-    $('#wrapper-2-13').toggle();
-});
+    	$('#close-button-2-13').click(function () {
+    		$('#wrapper-2-13').toggle();
+    	});
 
 
 
 
-//print 14
+    	//print 14
 
 
-        $('#print14js').click(function() {
-    $('#wrapper-2-14').toggle();
-});
+    	$('#print14js').click(function () {
+    		$('#wrapper-2-14').toggle();
+    	});
 
-        $('#close-button-2-14').click(function() {
-    $('#wrapper-2-14').toggle();
-});
+    	$('#close-button-2-14').click(function () {
+    		$('#wrapper-2-14').toggle();
+    	});
 
 
 
@@ -369,224 +369,246 @@
 
 
 
-//shirt
+    	//web
 
 
 
 
 
 
-//shirt 1
+    	//web 2
 
 
-        $('#shirt1js').click(function() {
-    $('#wrapper-3-1').toggle();
-});
+    	$('#web2js').click(function () {
+    		$('#wrapper-4-2').toggle();
+    	});
+			
+		
+    	$('#close-button-4-2').click(function () {
+    		$('#wrapper-4-2').toggle();
+    	});
 
-        $('#close-button-3-1').click(function() {
-    $('#wrapper-3-1').toggle();
-});
 
 
+    	//web 3
 
-//shirt 2
 
+    	$('#web3js').click(function () {
+    		$('#wrapper-4-3').toggle();
+    	});
 
-        $('#shirt2js').click(function() {
-    $('#wrapper-3-2').toggle();
-});
+    	$('#close-button-4-3').click(function () {
+    		$('#wrapper-4-3').toggle();
+    	});
 
-        $('#close-button-3-2').click(function() {
-    $('#wrapper-3-2').toggle();
-});
 
 
 
-//shirt 3
 
 
-        $('#shirt3js').click(function() {
-    $('#wrapper-3-3').toggle();
-});
 
-        $('#close-button-3-3').click(function() {
-    $('#wrapper-3-3').toggle();
-});
 
 
+    	//shirt
 
-//shirt 4
 
 
-        $('#shirt4js').click(function() {
-    $('#wrapper-3-4').toggle();
-});
 
-        $('#close-button-3-4').click(function() {
-    $('#wrapper-3-4').toggle();
-});
 
 
+    	//shirt 1
 
 
+    	$('#shirt1js').click(function () {
+    		$('#wrapper-3-1').toggle();
+    	});
 
+    	$('#close-button-3-1').click(function () {
+    		$('#wrapper-3-1').toggle();
+    	});
 
-//photo
 
 
+    	//shirt 2
 
 
+    	$('#shirt2js').click(function () {
+    		$('#wrapper-3-2').toggle();
+    	});
 
+    	$('#close-button-3-2').click(function () {
+    		$('#wrapper-3-2').toggle();
+    	});
 
-//photo 1
 
 
-        $('#photo1js').click(function() {
-    $('#wrapper-5-1').toggle();
-});
+    	//shirt 3
 
-        $('#close-button-5-1').click(function() {
-    $('#wrapper-5-1').toggle();
-});
 
+    	$('#shirt3js').click(function () {
+    		$('#wrapper-3-3').toggle();
+    	});
 
-//photo 2
+    	$('#close-button-3-3').click(function () {
+    		$('#wrapper-3-3').toggle();
+    	});
 
 
-        $('#photo2js').click(function() {
-    $('#wrapper-5-2').toggle();
-});
 
-        $('#close-button-5-2').click(function() {
-    $('#wrapper-5-2').toggle();
-});
+    	//shirt 4
 
 
-//photo 3
+    	$('#shirt4js').click(function () {
+    		$('#wrapper-3-4').toggle();
+    	});
 
+    	$('#close-button-3-4').click(function () {
+    		$('#wrapper-3-4').toggle();
+    	});
 
-        $('#photo3js').click(function() {
-    $('#wrapper-5-3').toggle();
-});
 
-        $('#close-button-5-3').click(function() {
-    $('#wrapper-5-3').toggle();
-});
 
 
-//photo 4
 
 
-        $('#photo4js').click(function() {
-    $('#wrapper-5-4').toggle();
-});
+    	//photo
 
-        $('#close-button-5-4').click(function() {
-    $('#wrapper-5-4').toggle();
-});
 
 
-//photo 5
 
 
-        $('#photo5js').click(function() {
-    $('#wrapper-5-5').toggle();
-});
 
-        $('#close-button-5-5').click(function() {
-    $('#wrapper-5-5').toggle();
-});
+    	//photo 1
 
 
-//photo 2
+    	$('#photo1js').click(function () {
+    		$('#wrapper-5-1').toggle();
+    	});
 
+    	$('#close-button-5-1').click(function () {
+    		$('#wrapper-5-1').toggle();
+    	});
 
-        $('#photo6js').click(function() {
-    $('#wrapper-5-6').toggle();
-});
 
-        $('#close-button-5-6').click(function() {
-    $('#wrapper-5-6').toggle();
-});
+    	//photo 2
 
 
-//photo 7
+    	$('#photo2js').click(function () {
+    		$('#wrapper-5-2').toggle();
+    	});
 
+    	$('#close-button-5-2').click(function () {
+    		$('#wrapper-5-2').toggle();
+    	});
 
-        $('#photo7js').click(function() {
-    $('#wrapper-5-7').toggle();
-});
 
-        $('#close-button-5-7').click(function() {
-    $('#wrapper-5-7').toggle();
-});
+    	//photo 3
 
 
-//photo 8
+    	$('#photo3js').click(function () {
+    		$('#wrapper-5-3').toggle();
+    	});
 
+    	$('#close-button-5-3').click(function () {
+    		$('#wrapper-5-3').toggle();
+    	});
 
-        $('#photo8js').click(function() {
-    $('#wrapper-5-8').toggle();
-});
 
-        $('#close-button-5-8').click(function() {
-    $('#wrapper-5-8').toggle();
-});
+    	//photo 4
 
 
-//photo 9
+    	$('#photo4js').click(function () {
+    		$('#wrapper-5-4').toggle();
+    	});
 
+    	$('#close-button-5-4').click(function () {
+    		$('#wrapper-5-4').toggle();
+    	});
 
-        $('#photo9js').click(function() {
-    $('#wrapper-5-9').toggle();
-});
 
-        $('#close-button-5-9').click(function() {
-    $('#wrapper-5-9').toggle();
-});
+    	//photo 5
 
 
+    	$('#photo5js').click(function () {
+    		$('#wrapper-5-5').toggle();
+    	});
 
+    	$('#close-button-5-5').click(function () {
+    		$('#wrapper-5-5').toggle();
+    	});
 
 
+    	//photo 2
 
 
+    	$('#photo6js').click(function () {
+    		$('#wrapper-5-6').toggle();
+    	});
 
+    	$('#close-button-5-6').click(function () {
+    		$('#wrapper-5-6').toggle();
+    	});
 
 
+    	//photo 7
 
 
+    	$('#photo7js').click(function () {
+    		$('#wrapper-5-7').toggle();
+    	});
 
+    	$('#close-button-5-7').click(function () {
+    		$('#wrapper-5-7').toggle();
+    	});
 
 
+    	//photo 8
 
 
+    	$('#photo8js').click(function () {
+    		$('#wrapper-5-8').toggle();
+    	});
 
+    	$('#close-button-5-8').click(function () {
+    		$('#wrapper-5-8').toggle();
+    	});
 
 
+    	//photo 9
 
 
+    	$('#photo9js').click(function () {
+    		$('#wrapper-5-9').toggle();
+    	});
 
+    	$('#close-button-5-9').click(function () {
+    		$('#wrapper-5-9').toggle();
+    	});
 
 
 
-        //window.onresize = function(){ location.reload(); }
 
-        if ( $(window).outerWidth() > 644) {
-  //Add your javascript for large screens here
-}
-else {
-   $('#menu-button').click(function() {
-    $('.nav').toggle();
-})
 
 
-        $('.nav-list-item-link').click(function() {
-    $('.nav').toggle();
-});
 
-}
 
 
+    	//window.onresize = function(){ location.reload(); }
 
-        });
+    	if ($(window).outerWidth() > 644) {
+    		//Add your javascript for large screens here
+    	} else {
+    		$('#menu-button').click(function () {
+    			$('.nav').toggle();
+    		})
+
+
+    		$('.nav-list-item-link').click(function () {
+    			$('.nav').toggle();
+    		});
+
+    	}
+
+
+
+    });
