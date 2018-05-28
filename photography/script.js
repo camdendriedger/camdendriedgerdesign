d6cead2bcd795025d29e1fcfe553198f59e471ae
Camden Driedger <camdendriedger@gmail.com>
2018-05-27 23:33:03 -0400

    All javascript works now!!

--- photography/script.js
+++ photography/script.js
@@ -1,11 +1,64 @@
-  
+$(document).ready(function() {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+				$(document).on("scroll", onScroll);
+
+				//smoothscroll
+				$('a[href^="#"]').on('click', function(e) {
+					e.preventDefault();
+					$(document).off("scroll");
+
+					$('a').each(function() {
+						$(this).removeClass('active');
+					})
+					$(this).addClass('active');
+
+					var target = this.hash,
+						menu = target;
+					$target = $(target);
+					$('html, body').stop().animate({
+						'scrollTop': $target.offset().top + 2
+					}, 500, 'swing', function() {
+						window.location.hash = target;
+						$(document).on("scroll", onScroll);
+					});
+				});
+			});
+
+			function onScroll(event) {
+				var scrollPos = $(document).scrollTop();
+				$('#nav-center a').each(function() {
+					var currLink = $(this);
+					var refElement = $(currLink.attr("href"));
+					if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+						$('#nav-center ul li a').removeClass("active");
+						currLink.addClass("active");
+					} else {
+						currLink.removeClass("active");
+					}
+				});
+			}  
 
 
 
 
 
 
-$(document).ready(function () {
+
+
+
+
+/*
+	
+	$(document).ready(function () {
 
 
 			// Add smooth scrolling to all links
@@ -31,7 +84,7 @@ $(document).ready(function () {
     		} // End if
     	});
 			
-			
+			*/
 			
 		
 		
@@ -103,14 +156,16 @@ $(function(){
   $('[data-scroll-speed]').moveIt();
 });
 		
-*/
+
 		
 
 		
 		
 		
 		
-
+$('#continue').click(function () {
+    		$('#important').toggle();
+    	});
 		
 		
 		
@@ -163,4 +218,14 @@ function showSlides(n) {
 		
 		
     });
+
+*/
 		
+
+
+
+
+
+
+
+
