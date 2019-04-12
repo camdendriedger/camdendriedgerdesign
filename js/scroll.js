41ebf6f71c1bef22864777e8cffd8d3b1296d435
Camden Driedger <camdendriedger@gmail.com>
2019-04-12 13:59:42 -0400

    Desktop Upload
    Site appears to be fully functional, but missing mobile site yet

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-+$(document).ready(function() {
-+  $(window).scroll(function() {
-+    if ($(this).scrollTop() > 0) {
-+      $('.arrow').css('opacity', 0);
-+    } else {
-+      $('.arrow').css('opacity', 1);
-+    }
-+  });
-+});
